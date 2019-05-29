80919510c30286094706557678da81dc4a15f270
Steven Meyer <Triadarcher@gmail.com>
2019-05-29 13:11:10 -0700

    Math drill  page 114

---
+++ classMethodDrill114/classMethodDrill114/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethodDrill114
+{
+    class Math
+    {
+        public int SameName(int num1)
+        {
+            int total1 = num1 - 1;
+            return total1;
+        }
+
+        public int SameName(double number)
+        {
+            double result = number * 10.5;
+            int total2 = Convert.ToInt32(result);
+            return total2;
+        }
+
+        public int SameName(string a)
+        {
+            int numA = Convert.ToInt32(a);
+            int total3 = numA + 1;
+            return total3;
+        }
+    }
+}
---
+++ classMethodDrill114/classMethodDrill114/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethodDrill114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n");                                                             //creates new line
+            Console.WriteLine("Math operation pg114");                                           //writes message to screen
+            Console.WriteLine("\n");                                                             //creates new line
+            Console.WriteLine("Press enter to continue...");                                     //writes message to screen
+            Console.ReadLine();                                                                  //waiting for user
+
+            Math Calc = new Math();                                                              //opens Math class and issigns to Calc variable
+
+
+            int total1 = Calc.SameName(12);                                                      //assigns int sameName to total1
+            Console.WriteLine("\n");                                                             //creates new line
+            Console.WriteLine("The result of integer 12 - 1 equals... " + total1);               //writes text and result to screen
+            Console.WriteLine("Press enter to continue...");                                     //writes message to screen
+            Console.ReadLine();                                                                  //waiting for user
+
+
+            int total2 = Calc.SameName(5.5);                                                     //assigns decimal sameName to total2
+            Console.WriteLine("\n");                                                             //creates new line
+            Console.WriteLine("The total multiplied by 10.5 equals... " + total2);               //writes text and result to screen
+            Console.WriteLine("Press enter to continue...");                                     //writes message to screen
+            Console.ReadLine();                                                                  //waiting for user
+
+
+            int total3 = Calc.SameName("25");                                                    //assigns string sameName to total3
+            Console.WriteLine("\n");                                                             //creates new line
+            Console.WriteLine("The string value + 1 equals... " + total3);                       //writes text and result to screen
+            Console.WriteLine("Press enter to exit...");                                         //writes message to screen
+            Console.ReadLine();                                                                  //waiting for user
+
+        }
+    }
+}
