6f1381069fab48f07b83c57b279c0658932aca1d
Steven Meyer <Triadarcher@gmail.com>
2019-05-30 12:19:28 -0700

    Drillpg120

---
+++ VoidMethoddrillpg120/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethoddrillpg120
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { FirstName = "John", LastName = "Doe" };
+            employee1.SayName();
+            Console.Read();
+        }
+    }
+}
---
+++ VoidMethoddrillpg120/VoidMethoddrillpg120/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethoddrillpg120
+{
+    class Person
+    {
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
