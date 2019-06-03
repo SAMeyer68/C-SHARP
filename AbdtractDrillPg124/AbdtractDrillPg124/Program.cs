053bb549f739725f716a1194fe5044b1c7000cec
Steven Meyer <Triadarcher@gmail.com>
2019-06-03 10:57:18 -0700

    Drill Page 124

---
+++ AbdtractDrillPg124/AbdtractDrillPg124/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbdtractDrillPg124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("AbdtractDrillPg124");
+            Console.WriteLine("\n");
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+            Console.WriteLine("\n");
+            Console.WriteLine("Hit enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
