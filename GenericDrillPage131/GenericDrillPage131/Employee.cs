b57b2bc98f957770b84c1e88d40c1b916316c019
Steven Meyer <Triadarcher@gmail.com>
2019-06-05 10:21:33 -0700

    DrillPage131

---
+++ GenericDrillPage131/GenericDrillPage131/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDrillPage131
+{
+    public class Employee<T> : Person, IQuittable
+
+    {
+
+        //GENERIC EXAMPLE
+
+        public List<T> Things { get; set; }
+     
+        //PERSON CLASS
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        //IQUITTABLE INTERFACE
+
+        public void Quit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GenericDrillPage131/GenericDrillPage131/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDrillPage131
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public virtual void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
