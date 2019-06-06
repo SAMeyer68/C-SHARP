2edd42db76eb242abbce91b5d5ec5a55b39bf2a8
Steven Meyer <Triadarcher@gmail.com>
2019-06-06 11:55:41 -0700

    StructDrillPage137

---
+++ StructDrillPage137/StructDrillPage137/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDrillPage137
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }    //get Amount from main and sets it to decimal Amount
+    }
+}
---
+++ StructDrillPage137/StructDrillPage137/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDrillPage137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number() { Amount = 9.2m };          //assigns 9.2 to Amount/converts to decimal in Number and assigns as new Number
+            Console.WriteLine("\n");                                 //new line
+            Console.WriteLine("The number is  " + number.Amount);    //writes to screen
+            Console.WriteLine("\n");                                 //new line
+            Console.Write("Press enter to exit...");                 //writes to screen
+            Console.WriteLine("\n");                                 //new line
+            Console.Read();
+        }
+    }
+}
