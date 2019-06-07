3f34a00d5c8d919f566767654665f667c2d21c64
Steven Meyer <Triadarcher@gmail.com>
2019-06-07 14:43:04 -0700

    Submit TwentyOne

---
+++ TwentyOne/TwentyOne/Deck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            this.Cards = new List<Card>();
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Face face in Enum.GetValues(typeof(Face)))
+                {
+                    Card card = new Card();
+                    card.Face = face;
+                    card.Suit = suit;
+                    this.Cards.Add(card);
+                }
+            }
+        }
+        public List<Card> Cards { get; set; }
+        public void Shuffle(int times = 1)
+        {
+            Random random = new Random();
+            for (int x = 0; x < times; x++)
+            {
+                List<Card> TempList = new List<Card>();
+                while (this.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, this.Cards.Count);
+                    TempList.Add(this.Cards[randomIndex]);
+                    this.Cards.RemoveAt(randomIndex);
+                }
+                this.Cards = TempList;
+            }
+        }
+    }
+}
---
+++ TwentyOne/TwentyOne/TwentyOneDealer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class TwentyOneDealer : Dealer
+    {
+        private List<Card> _Hand = new List<Card>();
+        public List<Card> Hand { get { return _Hand; } set { _Hand = value; } }
+        public bool Stay { get; set; }
+        public bool isBusted { get; set; }
+    }
+}
