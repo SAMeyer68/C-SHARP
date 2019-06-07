ffb44e70666a67f94eeee2060dc1518c377e8c5c
Steven Meyer <Triadarcher@gmail.com>
2019-06-07 15:39:48 -0700

    LogDrillPage154

---
+++ LogDrillPage154/LogDrillPage154/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogDrillPage154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Log Drill");
+            Console.Write("Please enter a number between 1 and 100:  ");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Steven\Desktop\TestLogDrillFolder\logdrill.txt", text);
+            string file = File.ReadAllText(@"C:\Users\Steven\Desktop\TestLogDrillFolder\logdrill.txt");
+            Console.WriteLine(file);
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
