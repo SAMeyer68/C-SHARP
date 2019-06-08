c80931743d95c35ddbfa3b6e1d234f45c51b7dbc
Steven Meyer <Triadarcher@gmail.com>
2019-06-07 17:23:10 -0700

    LogDrillPage154

--- LogDrillPage154/LogDrillPage154/Program.cs
+++ LogDrillPage154/LogDrillPage154/Program.cs
@@ -11,14 +11,14 @@ namespace LogDrillPage154
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Log Drill");
-            Console.Write("Please enter a number between 1 and 100:  ");
-            string text = Console.ReadLine();
-            File.WriteAllText(@"C:\Users\Steven\Desktop\TestLogDrillFolder\logdrill.txt", text);
-            string file = File.ReadAllText(@"C:\Users\Steven\Desktop\TestLogDrillFolder\logdrill.txt");
-            Console.WriteLine(file);
-            Console.WriteLine("Press enter to exit...");
-            Console.ReadLine();
+            Console.WriteLine("Log Drill");                                 //write to screen
+            Console.Write("Please enter a number between 1 and 1000:  ");    //write to screen
+            string text = Console.ReadLine();                               //takes user input and assigns it to text
+            File.WriteAllText(@"C:\Users\Steven\Desktop\TestLogDrillFolder\logdrill.txt", text); //writes text to folder at address
+            string file = File.ReadAllText(@"C:\Users\Steven\Desktop\TestLogDrillFolder\logdrill.txt"); //reads text in file at address and assigns it file
+            Console.WriteLine(file);                                         //writes file to screen
+            Console.WriteLine("Press enter to exit...");                     //write to screen
+            Console.ReadLine();                                              //waitin user to press enter
         }
     }
 }
