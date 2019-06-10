24b5317d575a463766a4207188663466116c13c4
Steven Meyer <Triadarcher@gmail.com>
2019-06-10 13:31:55 -0700

    ConstDrillPage162

---
+++ ConstChainDrillPage162/ConstChainDrillPage162/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstChainDrillPage162
+{
+    public class Class1
+    {
+        public Class1(string Tuckers) : this(50, Tuckers)             //Puts the two strings together
+        {
+        }
+        public Class1(int car, string name)
+        {
+            Cars = car;
+            TucksCars = name;
+        }
+        public int Cars { get; set; }
+        public string TucksCars { get; set; }
+    }
+}
---
+++ ConstChainDrillPage162/ConstChainDrillPage162/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstChainDrillPage162
+{
+    class Program
+    {
+        const string Key = "This Key String";
+        static void Main(string[] args)
+        {
+            const string carName = "Tuckers";                                                               //assigns const tuckers to carName
+            Class1 classOne = new Class1("Auto");                                                           //calls class1
+            var carsMade = classOne.Cars;                                                                   //assigns classOne
+            Console.WriteLine("Number of {0} cars made was {1}", carName, carsMade);                        //write to screen
+            Console.WriteLine("Press enter to exit...");                                                    //write to screen
+            Console.Read();                                                                                 //waiting for user to press enter
+        }
+    }
+}
